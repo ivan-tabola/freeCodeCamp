2ecbfaf85fdc6252fe2f34c169ff74dd702aa508
Ivan Tabola <j.tabola2016@yandex.by>
2023-07-26 00:30:26 +0300

    38.js

---
+++ BasicJavaScript/38.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "It grew so " + myAdverb + " into a " + myAdjective + " silhouette resembling a " + myNoun + ", but the desire to " + myVerb + " still remained " + "."; // Change this line
+// Only change code above this line
