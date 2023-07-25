711c2fb3da95dbe3923c134453b4761308dd26cd
Ivan Tabola <j.tabola2016@yandex.by>
2023-07-25 18:44:18 +0300

    32.js

---
+++ BasicJavaScript/32.js
@@ -0,0 +1,8 @@
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName;
+lastNameLength = 8;
+lastNameLength = lastName.length;
