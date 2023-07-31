99142c175e838582169c8cdfb662e6b5bd6c4622
Ivan Tabola <j.tabola2016@yandex.by>
2023-07-31 11:25:06 +0300

    82.js

---
+++ BasicJavaScript/82.js
@@ -0,0 +1,8 @@
+const myDog = {
+    // Only change code below this line
+    name: "Shaggy",
+    legs: 4,
+    tails: 1,
+    friends: ["Water", "Human"]
+    // Only change code above this line
+  };
