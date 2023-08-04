6a14a9318298e5fadc6adefb3c31cf454f524094
Ivan Tabola <j.tabola2016@yandex.by>
2023-08-04 18:33:45 +0300

    107.js

---
+++ BasicJavaScript/107.js
@@ -0,0 +1,11 @@
+function randomFraction() {
+
+    // Only change code below this line
+    let i = 0; 
+      while (i === 0) {
+      i = Math.random();
+    }
+    return i;
+  
+    // Only change code above this line
+  }
