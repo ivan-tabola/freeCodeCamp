4aa75e65b78ff043aea7e58af32ae49baf06c28a
Ivan Tabola <j.tabola2016@yandex.by>
2023-08-14 23:32:23 +0300

    07.js

---
+++ BasicDataStructures/06.js
@@ -0,0 +1,10 @@
+function htmlColorNames(arr) {
+    // Only change code below this line
+  const a = 0;
+  const b = 2;
+  arr.splice(a, b,'DarkSalmon','BlanchedAlmond');
+    // Only change code above this line
+    return arr;
+  }
+  
+  console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
